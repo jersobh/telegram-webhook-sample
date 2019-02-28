c229ff11790f4719f0744f840fb2a20865f093df
Jefferson de Andrade Santos <jersobh@users.noreply.github.com>
2019-02-28 13:17:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,37 +2,31 @@ from os import environ
 from flask import Flask, request, jsonify
 import requests
 
-# https://core.telegram.org/bots/api#setwebhook
-
 BOT_TOKEN = environ.get('TELEGRAM_BOT_TOKEN')
 WEBHOOK_URL = environ.get('TELEGRAM_WEBHOOK_URL')
-
-
 def set_webhook():
     set_webhook_url = f'https://api.telegram.org/bot{BOT_TOKEN}/setWebHook?url={WEBHOOK_URL}'
     webhook_response = requests.get(set_webhook_url)
     return webhook_response
 
-
 set_webhook_response = set_webhook()
-# if set_webhook_response.status_code != 200:
-#   raise ValueError('Set webhook fail')
-
 print(set_webhook_response.text)
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['POST'])
 def telegram():
     data_body = request.get_json()
     print(data_body)
     message = data_body.get('message')
+    print(message)
     username = message.get('from').get('username')
-    text = f"{message.get('text')} ... {username}"
+    print(username)
     chat_id = message.get('chat').get('id')
-    resp = requests.post(f'https://api.telegram.org/bot{BOT_TOKEN}/sendmessage?POST',
-                         json={'chat_id': chat_id, 'text': text})
+    answer = f"{username}: {message['text']}"
+
+    resp = requests.post(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?POST',
+                         json={'chat_id': chat_id, 'text': answer})
     return jsonify(resp.json()), resp.status_code
 
-# app.run()
+
